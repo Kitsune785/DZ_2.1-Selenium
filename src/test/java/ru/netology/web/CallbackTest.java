77214456b7578844413b802974d7ee58e3de2dcc
Anna <akhrolenko85@gmail.com>
2022-08-11 22:07:07 +0300

    доработка теста shouldInsertNonStandardLetters

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -50,17 +50,16 @@ public class CallbackTest {
         assertEquals(expected, actual);
     }
 
-//    @Test         // тест на Ё
-//    void shouldInsertNonStandardLetters() {
-
-//        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Семёнов Инокентий");
-//        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79993332222");
-//        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
-//        driver.findElement(By.className("button")).click();
-//        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-//        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
-//        assertEquals(expected, actual);
-//    }
+    @Test
+    void shouldInsertNonStandardLetters() {
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Семёнов Инокентий");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79993332222");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+        assertEquals(expected, actual);
+    }
 
     @Test
     void shouldShowIncorrectName() {
